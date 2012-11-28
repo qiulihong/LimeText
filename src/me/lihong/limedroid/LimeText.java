ea29bedcf0e41c869f0d05a5b513efab425ab7bf
Lihong <hi@lihong.me>
2012-11-28 21:02:59 +1100

    change styles to Holo

--- src/me/lihong/limedroid/LimeText.java
+++ src/me/lihong/limedroid/LimeText.java
@@ -3,6 +3,7 @@ package me.lihong.limedroid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class LimeText extends Activity {
 
@@ -17,6 +18,28 @@ public class LimeText extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_lime_text, menu);
         return true;
+    }
+    
+//    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch (item.getItemId()) {
+	    	case R.id.save:
+	    		save();
+	    		return true;
+	    	case R.id.save_as:
+	    		saveAs();
+	    		return true;
+	    	default:
+	    		return super.onOptionsItemSelected(item);
+    	}
+    }
+    
+    private boolean save() {
+    	return true;
+    }
+    
+    private boolean saveAs() {
+    	return true;
     }
     
 }
